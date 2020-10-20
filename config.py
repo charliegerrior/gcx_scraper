81862a42a55ab5aa21a8a94a62edc713dee8a465
Charles Gerrior <charles.gerrior@gmail.com>
2020-10-20 17:56:04 -0400

    works w/ Python 3.8

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     REDDIT_CLIENT_ID = os.environ.get('REDDIT_CLIENT_ID')
     REDDIT_CLIENT_SECRET = os.environ.get('REDDIT_CLIENT_SECRET')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:////home/charlie/gcxbot/webapp/app.db'
+        'sqlite:////Users/charlie/gcxbot/webapp/app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
 #'sqlite:///' + os.path.join(basedir, 'app.db')
